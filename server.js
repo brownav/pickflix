a90f46150933d443f30fe42249156ba3bafa9b56
Alexandria Brown <axbn2190@gmail.com>
2018-07-05 10:36:52 -0700

    server now running on port 3000

--- server.js
+++ server.js
@@ -5,8 +5,15 @@ const app = express();
 
 // db config
 const db = require('./config/keys').mongoURI;
+
 // connect to mongoDB
 mongoose
   .connect(db)
   .then(() => console.log('mongoDB connected'))
   .catch(err => console.log(err));
+
+app.get('/', (req, res) => res.send('This is Pickflix!'));
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
