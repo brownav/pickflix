1de9d61536e35684eb02fa3d0709a8f509311aa2
Alexandria Brown <axbn2190@gmail.com>
2018-07-06 11:49:26 -0700

    grabs and formats list of first 250 movies on reelgood into list

--- grabMovies.js
+++ grabMovies.js
@@ -1,27 +1,37 @@
 const axios = require('axios');
 const _ = require('lodash');
 
-const URL = "https://api.reelgood.com/v2/browse/source/netflix?take=250&skip=0&&year_start=1900&year_end=2018&availability=onSources&hide_seen=false&hide_tracked=false&hide_watchlisted=false&content_kind=both&sources=netflix&sort=0&free=false&override_user_sources=true&overriding_sources=netflix"
+let movieBatch = 250
+let URL = "https://api.reelgood.com/v2/browse/source/netflix?take=" + movieBatch + "&skip=0&&year_start=1900&year_end=2018&availability=onSources&hide_seen=false&hide_tracked=false&hide_watchlisted=false&content_kind=both&sources=netflix&sort=0&free=false&override_user_sources=true&overriding_sources=netflix"
 
-const listMovies = () => {
+// until the response is empty array, make request and increment movieBatch
+
+const grabMovies = () => {
   axios.get(URL)
     .then((response) => {
-
-    for (let i = 0; i < response.data.length; i++) {
-      let movie = _.pick(response.data[i], [
-        'title', 'content_type',
-        'episode_source_count'
-      ]);
-      console.log(movie);
-    }
+      movieList(response.data);
     })
     .catch((error) => {
       console.log(error);
     });
 }
 
+const movieList = (movies) => {
+  let movieList = [];
+  for (let i = 0; i < movies.length; i++) {
+    let movie = _.pick(movies[i], [
+      'title', 'content_type',
+      'episode_source_count'
+    ]);
+  movieList.push(movie);
+  }
+  console.log(`${movieList.length}`);
+}
+
+
 // // for page do
-const movieChunk = listMovies();
+const movieChunk = grabMovies();
+
 //   Movies.insertMany(movieChunk);
 
 //module.exports = {listMovies};
