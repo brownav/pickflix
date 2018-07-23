e33920da56404a97ad7faf35d54753014f5c7813
Alexandria Brown <axbn2190@gmail.com>
2018-07-23 14:18:58 -0700

    changed key

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,4 @@
 module.exports = {
-  mongoURI: "mongodb://heroku_qcjrrmz3:nktl54i0hd95chi87ui953ns4o@ds145911.mlab.com:45911/heroku_qcjrrmz3/pickaflick"
-  // mongoURI: "mongolab-octagonal-51435",
+  mongoURI: "mongolab-octagonal-51435",
   OMDB_KEY: "f8b7352"
 }
