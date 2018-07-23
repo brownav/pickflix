04cf4c4fa2679926cb919d72682f49c8978c149e
Alexandria Brown <axbn2190@gmail.com>
2018-07-23 14:26:45 -0700

    fixing mongouri

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,5 @@
 module.exports = {
-  mongoURI: "mongolab-octagonal-51435",
+  mongoURI: "mongodb://heroku_qcjrrmz3:nktl54i0hd95chi87ui953ns4o@ds145911.mlab.com:45911/heroku_qcjrrmz3"
+  // mongoURI: "mongolab-octagonal-51435",
   OMDB_KEY: "f8b7352"
 }
