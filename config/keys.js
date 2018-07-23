dc8d49b389efa6a24642daf7a8904ca7bcf942d0
Alexandria Brown <axbn2190@gmail.com>
2018-07-23 15:40:51 -0700

    missed a comma

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 module.exports = {
-  MONGODB_URI: "mongodb://heroku_qcjrrmz3:nktl54i0hd95chi87ui953ns4o@ds145911.mlab.com:45911/heroku_qcjrrmz3"
+  MONGODB_URI: "mongodb://heroku_qcjrrmz3:nktl54i0hd95chi87ui953ns4o@ds145911.mlab.com:45911/heroku_qcjrrmz3",
   OMDB_KEY: "f8b7352"
 }
 
