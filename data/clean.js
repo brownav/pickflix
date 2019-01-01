804d4f76b6ef77aef8175193cf98025dc95100f0
Alexandria Brown <axbn2190@gmail.com>
2018-12-31 22:19:26 -0800

    db population works

--- data/clean.js
+++ data/clean.js
@@ -1,19 +1,54 @@
 const axios = require('axios');
 const _ = require('lodash');
 const key = require('../config/keys').OMDB_KEY;
+const Movie = require('../models/Movie');
+const mongoose = require('mongoose');
+const db = require('../config/keys').MONGODB_URI;
+mongoose.connect(db, {
+  useNewUrlParser: true
+});
 
-
-async function saveMoviesToDB() {
+async function populateDBWithMovies() {
   try {
     const titles = await grabTitles();
     const rawTitles = await processTitleResults(titles)
     const fullMovies = await attachOMDBInfo(rawTitles);
     const cleanMovies = await cleanFullMovies(fullMovies);
-    return cleanMovies
+    return await saveMoviesToDB(cleanMovies);
   } catch (error) {
     console.log(error)
   }
 }
+
+function saveMoviesToDB(movies) {
+  let i = 0;
+  for(movie of movies) {
+    let newMovie = _.pick(movie,
+      ['title', 'avgRating', 'episode_source_count', 'Rated',
+      'Released', 'Runtime', 'Genre', 'Director', 'Actors', 'Plot',
+      'Awards', 'Poster', 'Ratings', 'Type'])
+    let x = Movie({
+       title: newMovie.title,
+       contentType: newMovie.Type,
+       episodeCount: newMovie.episode_source_count,
+       rated: newMovie.Rated,
+       released: newMovie.Released,
+       genres: newMovie.Genre,
+       plot: newMovie.Plot,
+       awards: newMovie.Awards,
+       image: newMovie.Poster,
+       ratings: newMovie.Ratings,
+       avgRating: newMovie.avgRating,
+       director: newMovie.Director,
+       actors: newMovie.Actors,
+       runtime: newMovie.Runtime,
+    })
+    x.save()
+    i++;
+    console.log(i, x.title);
+  }
+}
+
 
 function makeAverageRating(movieInfo) {
   if (movieInfo.Ratings) {
@@ -63,24 +98,28 @@ function cleanFullMovies(movies) {
   return cleanMovies;
 }
 
+
+
 async function attachOMDBInfo(titles) {
   let results = [];
-  for(title of titles) {
+  for (title of titles) {
     const URL = 'http://www.omdbapi.com/?apikey=' + key + '&t=' + encodeURI(title.title) + '&plot=short&r=json&y=' + title.released_on;
-    let result = await axios.get(URL)
+    let result = await axios.get(URL);
     let mergedMovie = _.merge(result.data, title);
+    console.log(mergedMovie.title)
     results.push(mergedMovie);
   }
   return results;
-}
+}   
+
 
 function grabTitles() {
   let skip = 0;
-  let i = 20;
+  let i = 0;
   let promises = [];
   while (i < 21) {
-    promises.push(axios.get("https://api.reelgood.com/v2/browse/source/netflix?take=5&skip=" + skip + "&&year_start=1900&year_end=2018&availability=onSources&hide_seen=false&hide_tracked=false&hide_watchlisted=false&content_kind=both&sources=netflix&sort=0&free=false&override_user_sources=true&overriding_sources=netflix"));
-    skip += 5;
+    promises.push(axios.get("https://api.reelgood.com/v2/browse/source/netflix?take=250&skip=" + skip + "&&year_start=1900&year_end=2018&availability=onSources&hide_seen=false&hide_tracked=false&hide_watchlisted=false&content_kind=both&sources=netflix&sort=0&free=false&override_user_sources=true&overriding_sources=netflix"));
+    skip += 250;
     i++;
   }
   return axios.all(promises);
@@ -96,7 +135,7 @@ function processTitleResults(titles) {
       movie.released_on = movie.released_on.slice(0,4);
       // accounts for using OMDB to call Marvel titles
       if (movie.title.indexOf('Marvel\'s') > -1) {
-        title = title.slice(8);
+        movie.title = movie.title.slice(8);
       }
       rawMovies.push(movie)
     })
@@ -104,6 +143,6 @@ function processTitleResults(titles) {
   return rawMovies;
 }
 
-saveMoviesToDB().then((result) => {
+populateDBWithMovies().then((result) => {
   console.log(result)
 })
